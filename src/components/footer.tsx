48d8be4e73a5301527faef4453bfad4e4682593c
Gabriel Oliveira <77246516+gabrieldeoliveira04@users.noreply.github.com>
2025-07-17 02:29:16 +0000

    .

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Copyright &copy; 2025 by Gabriel de Oliveira Gomes. All rights reserved.
+      </p>
+    </footer>
+  );
+}
